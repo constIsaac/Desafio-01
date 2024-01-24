db1cff415fac00b86a0b62bf3e61aaf54fa0b77f
constIsaac <isaacenzomd@gmail.com>
2024-01-24 15:46:15 -0400

    Projeto 01

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("oi")
+let herói = "Luke Skywalker"
+let xpHerói = 9235
+let PatenteDoHéroi;
+
+if (xpHerói <= 1000){
+console.log(PatenteDoHéroi = "Ferro")}
+else if (xpHerói >= 1001 && xpHerói <= 2000) {
+  console.log(PatenteDoHéroi = "Bronze")}
+else if (xpHerói >= 2001 && xpHerói <= 5000) {
+console.log(PatenteDoHéroi = "Prata")}
+else if (xpHerói >= 5001 && xpHerói <= 7000) {
+    console.log(PatenteDoHéroi = "Ouro")}
+else if (xpHerói >= 7001 && xpHerói <= 8000) {
+    console.log(PatenteDoHéroi = "Platina")}
+else if (xpHerói >= 8001 && xpHerói <= 9000) {
+    console.log(PatenteDoHéroi = "Ascendente")}
+else if (xpHerói >= 9001 && xpHerói <= 10.001) {
+    console.log(PatenteDoHéroi = "Imortal")}
+else {console.log (PatenteDoHéroi = "Radiante")}
+    
+console.log("O Herói de nome " + herói + " está no nível de " + PatenteDoHéroi)
